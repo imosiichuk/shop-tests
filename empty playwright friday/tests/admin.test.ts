97d88a906cc40f811668e2b42d2e6d6f67b1a04d
ivan <vanyamosiychuk@gmail.com>
2024-11-25 18:39:55 +0100

    admin test changes

--- empty playwright friday/tests/admin.test.ts
+++ empty playwright friday/tests/admin.test.ts
@@ -4,7 +4,7 @@ shopTestLoggedUser(
   "Admin should see additional items in dashboard",
   async ({ app: { dashboard }, adminUser }) => {
     await dashboard.open();
-    await dashboard.expectedMenuItemVisible("Reviews");
+    await dashboard.expectedMenuItemVisible("Reviewsaskdnkdajdakdaskjksdj");
     await dashboard.expectedMenuItemVisible("Orders");
     await dashboard.expectedMenuItemVisible("Users");
   }
